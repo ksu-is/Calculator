a56e627eb6a20f2e878a42cfc0667a7933841795
Xma8 <48929721+Xma8@users.noreply.github.com>
2019-03-27 12:00:40 -0400

    Add files via upload

---
+++ whileloop + eval.py
@@ -0,0 +1,12 @@
+on = True
+
+while on == True:
+    line = (input("Type in your equation here: "))
+    print("Answer: ",eval(line))
+'''   try:
+        line == float
+    except TypeError:
+        print("This is not a valid number!")'''
+
+
+# For this program, you're adding a whileloop, with eval!
